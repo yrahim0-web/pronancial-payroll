6ddf821b03fbd0406f60ce55738c9e2b921910b9
Yasir Rahim <yrahim0@gmail.com>
2026-05-22 12:15:19 +0500

    Use environment variables for Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+jsimport { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://aferijqnrpzccyuusoqm.supabase.co'
-const supabaseKey = 'sb_publishable_kYzc1GJGxHyNDqsAHCM9Xw_Q-z84nGa'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
