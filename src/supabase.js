fa5e245ba395bf9540211bf909aaa9812cbe1694
Yasir Rahim <yrahim0@gmail.com>
2026-05-22 12:22:32 +0500

    Fix supabase import typo

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,4 @@
-jsimport { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
